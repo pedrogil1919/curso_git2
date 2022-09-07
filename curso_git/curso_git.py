462776d022d79098c972c0f8a337aa84ec46fe66
pedro.gil <pedro.gil@DESKTOP-P12KTJ8.home>
2022-09-07 19:19:36 +0200

    Funciones de desplazamiento

--- curso_git/curso_git.py
+++ curso_git/curso_git.py
@@ -4,6 +4,20 @@ Created on 7 sept 2022
 @author: pedro.gil
 '''
 
+def avanzar(x, y, a):
+    if a == 'N': y += 1
+    elif a == 'S': y -= 1
+    elif a == 'E': x += 1
+    elif a == 'O': x -= 1
+    return x, y, a
+    
+def retroceder(x, y, a):
+    if a == 'N': y -= 1
+    elif a == 'S': y += 1
+    elif a == 'E': x -= 1
+    elif a == 'O': x += 1
+    return x, y, a
+
 x = 0
 y = 0
 a = 'N'
