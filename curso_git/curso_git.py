dcf349f13093f7f5c799956406e067f05f397f7e
pedro.gil <pedro.gil@DESKTOP-P12KTJ8.home>
2022-09-07 19:13:09 +0200

    Crear archivo principal y bucle para leer instrucciones

---
+++ curso_git/curso_git.py
@@ -0,0 +1,15 @@
+'''
+Created on 7 sept 2022
+
+@author: pedro.gil
+'''
+
+x = 0
+y = 0
+a = 'N'
+    
+while True:
+    
+    instruccion = input("Siguiente instrucción:")
+    if instruccion == "Q":
+        break
